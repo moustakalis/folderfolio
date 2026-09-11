206a1550cae22e58b3c6428d8d8b7376d934fc77
Nickos Moustakas <nickos.moustakas@gmail.com>
2026-09-11 12:10:21 +0300

    Move Plugin bootstrap to includes/

---
+++ includes/Plugin.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FolderFolio;
+
+/**
+ * Main plugin class.
+ */
+final class Plugin
+{
+    /**
+     * Initialize the plugin.
+     *
+     * @return void
+     */
+    public static function init(): void
+    {
+        // TODO: Add plugin initialization logic here.
+    }
+}
