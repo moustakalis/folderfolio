43f5f86639b7fd8fd7182c7a8ec12c294a05c012
Nickos Moustakas <nickos.moustakas@gmail.com>
2026-09-11 12:10:30 +0300

    Move Database classes to includes/Database/

---
+++ includes/Database/Database.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FolderFolio\Database;
+
+/**
+ * Database handler.
+ */
+final class Database
+{
+    /**
+     * Register database hooks.
+     *
+     * @return void
+     */
+    public static function register(): void
+    {
+        // TODO: Add database registration logic here.
+    }
+}
