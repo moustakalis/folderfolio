c525cc6596352f4dd4aa7d33682e3fb5345d24ab
Nickos Moustakas <nickos.moustakas@gmail.com>
2026-09-11 12:10:26 +0300

    Move Admin classes to includes/Admin/

---
+++ includes/Admin/AdminPage.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FolderFolio\Admin;
+
+/**
+ * Admin page handler.
+ */
+final class AdminPage
+{
+    /**
+     * Register admin hooks.
+     *
+     * @return void
+     */
+    public static function register(): void
+    {
+        // TODO: Add admin page registration logic here.
+    }
+}
