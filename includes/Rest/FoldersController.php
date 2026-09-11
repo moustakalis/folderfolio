596492f6b0e2c3f5762b1925c79480fd1b5f688f
Nickos Moustakas <nickos.moustakas@gmail.com>
2026-09-11 12:10:45 +0300

    Move Rest classes to includes/Rest/

---
+++ includes/Rest/FoldersController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FolderFolio\Rest;
+
+/**
+ * REST controller for folder operations.
+ */
+final class FoldersController
+{
+    /**
+     * Register REST routes.
+     *
+     * @return void
+     */
+    public static function register(): void
+    {
+        // TODO: Add REST routes for folders here.
+    }
+}
