8fb7d38aa362467ce85e72fce917b12f82c0d18a
Nickos Moustakas <nickos.moustakas@gmail.com>
2026-09-11 12:10:41 +0300

    Move Modules classes to includes/Modules/

---
+++ includes/Modules/FoldersModule.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FolderFolio\Modules;
+
+/**
+ * Folders module.
+ */
+final class FoldersModule
+{
+    /**
+     * Register module hooks.
+     *
+     * @return void
+     */
+    public static function register(): void
+    {
+        // TODO: Add folders module logic here.
+    }
+}
